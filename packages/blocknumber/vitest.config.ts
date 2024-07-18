9802a896ca500ed5f9cf685765d3b6a8716c6841
Yaco 0x <yaco@defi.sucks>
2024-07-18 18:19:49 -0300

    feat: set up blocknumber module tests

---
+++ packages/blocknumber/vitest.config.ts
@@ -0,0 +1,21 @@
+import path from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+    test: {
+        globals: true, // Use Vitest's global API without importing it in each file
+        environment: "node", // Use the Node.js environment
+        include: ["src/**/*.spec.ts"], // Include test files
+        exclude: ["node_modules", "dist"], // Exclude certain directories
+        coverage: {
+            reporter: ["text", "json", "html"], // Coverage reporters
+            exclude: ["node_modules", "dist", "src/**/*.d.ts"], // Files to exclude from coverage
+        },
+    },
+    resolve: {
+        alias: {
+            // Setup path alias based on tsconfig paths
+            "@": path.resolve(__dirname, "src"),
+        },
+    },
+});
