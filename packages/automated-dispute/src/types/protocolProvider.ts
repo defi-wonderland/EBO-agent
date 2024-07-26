bec87f3ec0c31f1168a452d96b320fb44d308f05
0xkenj1 <kenji@defi.sucks>
2024-07-26 15:28:24 -0300

    fix: pr comments

--- packages/automated-dispute/src/types/protocolProvider.ts
+++ packages/automated-dispute/src/types/protocolProvider.ts
@@ -1,5 +1,6 @@
+import { Address } from "viem";
+
 import { ProtocolContractsNames } from "../constants.js";
-import { Address } from "./index.js";
 
 export type ProtocolContract = (typeof ProtocolContractsNames)[number];
 export type ProtocolContractsAddresses = Record<ProtocolContract, Address>;
