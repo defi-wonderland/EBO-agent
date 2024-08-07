be694f168b989b66d1ed3aa2434940cfd3125d41
Yaco 0x <yaco@defi.sucks>
2024-08-07 11:48:09 -0300

    refactor: simplify eboMemoryRegistry constructor

--- packages/automated-dispute/src/eboMemoryRegistry.ts
+++ packages/automated-dispute/src/eboMemoryRegistry.ts
@@ -2,15 +2,11 @@ import { EboRegistry } from "./interfaces/eboRegistry.js";
 import { Dispute, Request, Response } from "./types/prophet.js";
 
 export class EboMemoryRegistry implements EboRegistry {
-    private requests: Map<string, Request>;
-    private responses: Map<string, Response>;
-    private dispute: Map<string, Dispute>;
-
-    constructor() {
-        this.requests = new Map();
-        this.responses = new Map();
-        this.dispute = new Map();
-    }
+    constructor(
+        private requests: Map<string, Request> = new Map(),
+        private responses: Map<string, Response> = new Map(),
+        private dispute: Map<string, Dispute> = new Map(),
+    ) {}
 
     /** @inheritdoc */
     public addRequest(requestId: string, request: Request) {
