75dc3643cefafa22a8264813dd46cf387c507c34
jahabeebs <47253537+jahabeebs@users.noreply.github.com>
2024-09-18 15:00:41 -0500

    feat: add poc

--- packages/automated-dispute/src/exceptions/errorFactory.ts
+++ packages/automated-dispute/src/exceptions/errorFactory.ts
@@ -1,17 +1,80 @@
 import { CustomContractError } from "../exceptions/index.js";
 import { ErrorHandlingStrategy } from "../types/index.js";
 
-const errorStrategies: Map<string, ErrorHandlingStrategy> = new Map([
-    //TODO: add all error strategies
+const errorStrategiesEntries: [string, ErrorHandlingStrategy][] = [
     [
-        "EBORequestCreator_InvalidEpoch",
+        "ValidatorLib_InvalidResponseBody",
         {
             shouldNotify: true,
             shouldTerminate: true,
             shouldConsume: false,
         },
     ],
-]);
+    [
+        "BondEscalationAccounting_InsufficientFunds",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "BondEscalationAccounting_AlreadySettled",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondEscalationModule_InvalidDispute",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: true,
+            customAction: async (context) => {
+                context.registry.removeDispute(context.dispute.id);
+            },
+        },
+    ],
+    [
+        "AccountingExtension_InsufficientFunds",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "Oracle_InvalidDisputeId",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: true,
+            customAction: async (context) => {
+                context.registry.removeDispute(context.dispute.id);
+            },
+        },
+    ],
+    [
+        "Oracle_AlreadyFinalized",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "EBORequestModule_InvalidRequester",
+        {
+            shouldNotify: true,
+            shouldTerminate: true,
+            shouldConsume: false,
+        },
+    ],
+];
+
+const errorStrategies = new Map<string, ErrorHandlingStrategy>(errorStrategiesEntries);
 
 export class ErrorFactory {
     public static createError(errorName: string): CustomContractError {
