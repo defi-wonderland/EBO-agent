484facd692ac0361b410abd791d338f669050890
jahabeebs <47253537+jahabeebs@users.noreply.github.com>
2024-09-18 19:10:41 -0500

    chore: add all errors
    Signed-off-by: jahabeebs <47253537+jahabeebs@users.noreply.github.com>

--- packages/automated-dispute/src/exceptions/errorFactory.ts
+++ packages/automated-dispute/src/exceptions/errorFactory.ts
@@ -5,9 +5,9 @@ const errorStrategiesEntries: [string, ErrorHandlingStrategy][] = [
     [
         "ValidatorLib_InvalidResponseBody",
         {
-            shouldNotify: true,
-            shouldTerminate: true,
-            shouldConsume: false,
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
         },
     ],
     [
@@ -57,6 +57,25 @@ const errorStrategiesEntries: [string, ErrorHandlingStrategy][] = [
         },
     ],
     [
+        "Oracle_InvalidDispute",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+            customAction: async (context) => {
+                context.registry.removeDispute(context.dispute.id);
+            },
+        },
+    ],
+    [
+        "Oracle_InvalidDisputeBody",
+        {
+            shouldNotify: true,
+            shouldTerminate: true,
+            shouldConsume: false,
+        },
+    ],
+    [
         "Oracle_AlreadyFinalized",
         {
             shouldNotify: false,
@@ -68,8 +87,258 @@ const errorStrategiesEntries: [string, ErrorHandlingStrategy][] = [
         "EBORequestModule_InvalidRequester",
         {
             shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "EBORequestModule_ChainNotAdded",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "EBORequestCreator_InvalidEpoch",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "Oracle_InvalidRequestBody",
+        {
+            shouldNotify: true,
             shouldTerminate: true,
             shouldConsume: false,
+        },
+    ],
+    [
+        "EBORequestCreator_RequestAlreadyCreated",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "Oracle_InvalidRequest",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "Oracle_InvalidResponseBody",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "AccountingExtension_NotAllowed",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "BondedResponseModule_AlreadyResponded",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondedResponseModule_TooLateToPropose",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "Oracle_CannotEscalate",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "Validator_InvalidDispute",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: true,
+            customAction: async (context) => {
+                context.registry.removeDispute(context.dispute.id);
+            },
+        },
+    ],
+    [
+        "ValidatorLib_InvalidDisputeBody",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: true,
+            customAction: async (context) => {
+                context.registry.removeDispute(context.dispute.id);
+            },
+        },
+    ],
+    [
+        "EBOFinalityModule_InvalidRequester",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "Oracle_InvalidFinalizedResponse",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "Oracle_InvalidResponse",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "Oracle_FinalizableResponseExists",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "ArbitratorModule_InvalidArbitrator",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: true,
+            customAction: async (context) => {
+                context.registry.removeDispute(context.dispute.id);
+            },
+        },
+    ],
+    [
+        "AccountingExtension_UnauthorizedModule",
+        {
+            shouldNotify: true,
+            shouldTerminate: true,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "BondEscalationModule_NotEscalatable",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondEscalationModule_BondEscalationNotOver",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondEscalationModule_BondEscalationOver",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondEscalationModule_DisputeWindowOver",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "Oracle_ResponseAlreadyDisputed",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondEscalationModule_CannotBreakTieDuringTyingBuffer",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondEscalationModule_CanOnlySurpassByOnePledge",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondEscalationModule_MaxNumberOfEscalationsReached",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "Oracle_NotDisputeOrResolutionModule",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+        },
+    ],
+    [
+        "BondEscalationModule_ShouldBeEscalated",
+        {
+            shouldNotify: true,
+            shouldTerminate: false,
+            shouldConsume: false,
+            // Custom action to escalate dispute is implemented in eboActor.ts
+        },
+    ],
+    [
+        "BondEscalationModule_BondEscalationCantBeSettled",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
+        },
+    ],
+    [
+        "BondEscalationModule_BondEscalationNotOver",
+        {
+            shouldNotify: false,
+            shouldTerminate: false,
+            shouldConsume: true,
         },
     ],
 ];
