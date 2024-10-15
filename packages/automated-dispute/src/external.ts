4410fbcb0742fc499bea252523ed9561d7045592
Yaco 0x <yaco@defi.sucks>
2024-10-15 11:09:53 +0200

    refactor: expose DiscordNotifier in automated dispute pkg

--- packages/automated-dispute/src/external.ts
+++ packages/automated-dispute/src/external.ts
@@ -1,4 +1,4 @@
-export { EboProcessor, EboActorsManager } from "./services/index.js";
+export { EboProcessor, EboActorsManager, DiscordNotifier } from "./services/index.js";
 export type { NotificationService } from "./interfaces/index.js";
 export { ProtocolProvider } from "./providers/index.js";
 export type { AccountingModules } from "./types/index.js";
